24d1fc47b5043b49d36f5baf3696e738c85e1aad
sharoa119 <sau.rin1109@gmail.com>
2023-10-19 10:45:56 +0900

    再度修正しました。

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,4 @@ Rails.application.routes.draw do
   scope '(:locale)', locale: /#{I18n.available_locales.map(&:to_s).join('|')}/ do
     resources :books
   end
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
