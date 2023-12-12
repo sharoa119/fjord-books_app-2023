757ab0ab4203c7542a81652f7d8ba569f3176e21
sharoa119 <sau.rin1109@gmail.com>
2023-12-12 11:21:34 +0900

    コントローラーを追加、画像を表示させるためのフォームなどを追加。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,24 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    @user.image.attach(params[:user][:image])
+    @user.save
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :name, :postal_code, :address, :self_introduction, :avatar)
+  end
 end
