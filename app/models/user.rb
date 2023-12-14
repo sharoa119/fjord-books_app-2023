2c3a53f2885c6cb607a112675faf1dbba0b913e6
sharoa119 <sau.rin1109@gmail.com>
2023-12-14 16:41:34 +0900

    必要だったgemの追加と、コントローラーの変更、バリデーションの追加などをした。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+  validate :avatar_content_type
+
+  def avatar_content_type
+    return unless avatar.attached? && !avatar.content_type.in?(%w[image/jpeg image/png image/gif])
+
+    errors.add(:avatar, ':ファイルは、JPEG、JPG、PNG、GIFのいずれかの形式でお願いします。')
+  end
 end
