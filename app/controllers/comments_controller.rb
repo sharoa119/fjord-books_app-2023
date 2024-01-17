410bd6792dde8ab5d9a906d66fe91bb9fbeaf386
sharoa119 <sau.rin1109@gmail.com>
2024-01-17 12:09:38 +0900

    reportとbooksのそれぞれのコメントコントローラーを作る。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
-  before_action :set_commentable
   before_action :set_comment, only: %i[destroy]
 
   # GET /comments/new
@@ -11,19 +10,12 @@ class CommentsController < ApplicationController
 
   # POST /comments or /comments.json
   def create
-    begin
-      @comment = @commentable.comments.build(comment_params)
-      @comment.user_id = current_user.id
+    @comment = @commentable.comments.build(comment_params)
+    @comment.user_id = current_user.id
 
-      ActiveRecord::Base.transaction do
-        @comment.save!
-      end
-
-      flash[:success] = 'コメントしました'
+    if @comment.save
       redirect_to @commentable, notice: 'Comment was successfully created.'
-    rescue => e
-      Rails.logger.debug e.message
-      flash[:error] = 'コメントできませんでした'
+    else
       render :new
     end
   end
@@ -36,15 +28,15 @@ class CommentsController < ApplicationController
 
   private
 
-  def set_commentable
-    if params[:report_id]
-      @commentable = Report.find_by(id: params[:report_id])
-    elsif params[:book_id]
-      @commentable = Book.find_by(id: params[:book_id])
-    end
+  # def set_commentable
+  #   if params[:report_id]
+  #     @commentable = Report.find_by(id: params[:report_id])
+  #   elsif params[:book_id]
+  #     @commentable = Book.find_by(id: params[:book_id])
+  #   end
 
-    redirect_to(reports_path, alert: 'Commentable not found') unless @commentable
-  end
+  #   redirect_to(reports_path, alert: 'Commentable not found') unless @commentable
+  # end
 
   def set_comment
     @comment = @commentable.comments.find(params[:id])
