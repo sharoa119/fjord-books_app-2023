ec84f29a4b9d2734538012c9b84e48e4cf490612
sharoa119 <sau.rin1109@gmail.com>
2024-02-13 14:23:24 +0900

    commentsコントローラのdestroyの箇所を修正しました。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
-  before_action :authorize_comment_user, only: [:destroy]
   # GET /comments/new
   def new
     @comment = @commentable.comments.build
@@ -22,17 +21,15 @@ class CommentsController < ApplicationController
   def destroy
     @comment = @commentable.comments.find(params[:id])
 
-    @comment.destroy
-    redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+    if @comment.present? && current_user == @comment.user
+      @comment.destroy
+      redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+    else
+      redirect_to @commentable, alert: t('controllers.common.alert')
+    end
   end
 
   private
-
-  def authorize_comment_user
-    return if @comment.nil? || current_user == @comment.user
-
-    redirect_to @commentable, alert: t('controllers.common.alert')
-  end
 
   def comment_params
     params.require(:comment).permit(:content)
