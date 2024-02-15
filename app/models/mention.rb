17a3150e9785d53c86b6daa953069f7fc2fc5033
sharoa119 <sau.rin1109@gmail.com>
2024-02-15 11:04:53 +0900

    reportsモデルの編集と、rubocopで指摘された箇所の修正、mentionビューの作り直し

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -2,9 +2,9 @@
 
 class Mention < ApplicationRecord
   # mentionする側
-  belongs_to :mentioning_report, class_name: 'Report', foreign_key: 'mentioning_report_id', inverse_of: :mentioning_relationships
+  belongs_to :mentioning_report, class_name: 'Report', inverse_of: :mentioning_relationships
   # mentionされる側
-  belongs_to :mentioned_report, class_name: 'Report', foreign_key: 'mentioned_report_id', inverse_of: :mentioned_relationships
+  belongs_to :mentioned_report, class_name: 'Report', inverse_of: :mentioned_relationships
 
   validates :mentioning_report_id, uniqueness: { scope: :mentioned_report_id }
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -8,13 +8,13 @@ class Report < ApplicationRecord
   validates :content, presence: true
 
   # mentionする側
-  has_many :mentioning_relationships, class_name: 'Mention', foreign_key: 'mentioning_report_id', dependent: :destroy
+  has_many :mentioning_relationships, class_name: 'Mention', foreign_key: 'mentioning_report_id', dependent: :destroy, inverse_of: :mentioning_report
   # その日報が言及している日報たち（言及先）
   # has_many ：関連名, through: :中間テーブル名, sources: :取得したい関連先
   has_many :mentioning_reports, through: :mentioning_relationships, source: :mentioned_report
 
   # mentionされる側
-  has_many :mentioned_relationships, class_name: 'Mention', foreign_key: 'mentioned_report_id', dependent: :destroy
+  has_many :mentioned_relationships, class_name: 'Mention', foreign_key: 'mentioned_report_id', dependent: :destroy, inverse_of: :mentioned_report
   # その日報に言及している日報たち（言及元）
   # has_many ：関連名, through: :中間テーブル名, sources: :取得したい関連先
   has_many :mentioned_reports, through: :mentioned_relationships, source: :mentioning_report
@@ -30,16 +30,33 @@ class Report < ApplicationRecord
     created_at.to_date
   end
 
+  def save_with_mentions
+    # トランザクションを開始
+    ActiveRecord::Base.transaction do
+      # 日報を保存
+      save!
+
+      # 言及データを保存
+      detect_mentions
+    end
+  rescue StandardError => e
+    # トランザクション内でのエラーが発生した場合の処理
+    # エラーのハンドリングやログ出力などが必要
+    logger.error "Failed to save report with mentions: #{e.message}"
+    raise e
+  end
+
   private
 
   def detect_mentions
     # 正規表現パターンを定義（http://localhost:3000/reports/123 のようなURLを抽出）
     url_pattern = %r{(http|https)://(?:localhost|127\.0\.0\.1):3000/reports/(\d+)}
 
-    # 本文テキストからURLを抽出
-    mentioned_report_ids = content.scan(url_pattern).map { |match| match[0].to_i }
-
-    # 取得した日報IDを self.mentioned_reports に保存する
-    self.mentioned_reports = mentioned_report_ids
+    # 本文テキストからURLを抽出し、それぞれの日報を取得して mentioned_reports に追加する
+    content.scan(url_pattern) do |match|
+      report_id = match[1].to_i
+      report = Report.find_by(id: report_id)
+      mentioned_reports << report if report
+    end
   end
 end
