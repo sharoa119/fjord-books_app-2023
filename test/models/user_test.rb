4f191d49340d3fb895614fe97968bec92a5db115
sharoa119 <sau.rin1109@gmail.com>
2024-08-27 14:41:48 +0900

    Userクラスのテストを書く

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,27 @@
-# frozen_string_literal: true
-
-require 'test_helper'
+require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#name_or_email" do
+    user = User.new(email: 'test_user_foo@example.com', name: '', password: 'password')
+    assert_equal 'test_user_foo@example.com', user.name_or_email
+
+    user.name = 'user Foo'
+    assert_equal 'user Foo', user.name_or_email
+  end
+
+  test "should not save user without email" do
+    user = User.new(name: 'user Foo', password: 'password')
+    assert_not user.save
+  end
+
+  test "should not save user without password" do
+    user = User.new(name: 'user Foo', email: 'test_user_foo@example.com')
+    assert_not user.save
+  end
+
+  test "should not save user with duplicate email" do
+    user1 = User.create(name: 'User Foo', email: 'test_user_foo@example.com', password: 'password')
+    user2 = User.new(name: 'User Bar', email: 'test_user_foo@example.com', password: 'password')
+    assert_not user2.save
+  end
 end
