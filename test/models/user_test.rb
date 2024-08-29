2291c678c29f13c7c20ca69b7d4209c051b5712f
sharoa119 <sau.rin1109@gmail.com>
2024-08-29 15:25:14 +0900

    rubocopで指摘されたダブルクオートをシングルに変更

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "#name_or_email" do
+  test '#name_or_email' do
     user = User.new(email: 'test_user_foo@example.com', name: '', password: 'password')
     assert_equal 'test_user_foo@example.com', user.name_or_email
 
@@ -9,17 +9,17 @@ class UserTest < ActiveSupport::TestCase
     assert_equal 'user Foo', user.name_or_email
   end
 
-  test "should not save user without email" do
+  test 'should not save user without email' do
     user = User.new(name: 'user Foo', password: 'password')
     assert_not user.save
   end
 
-  test "should not save user without password" do
+  test 'should not save user without password' do
     user = User.new(name: 'user Foo', email: 'test_user_foo@example.com')
     assert_not user.save
   end
 
-  test "should not save user with duplicate email" do
+  test 'should not save user with duplicate email' do
     user1 = User.create(name: 'User Foo', email: 'test_user_foo@example.com', password: 'password')
     user2 = User.new(name: 'User Bar', email: 'test_user_foo@example.com', password: 'password')
     assert_not user2.save
