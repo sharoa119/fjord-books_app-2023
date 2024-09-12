ce2759d13d37b8192f0574c49d66d2fc5bf5779e
sharoa119 <sau.rin1109@gmail.com>
2024-09-12 12:25:01 +0900

    save_mentionsのテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  fixtures :users
+  fixtures :users, :reports
 
   def setup
     @user1 = users(:user_foo)
@@ -30,5 +30,37 @@ class ReportTest < ActiveSupport::TestCase
 
   test 'should belong to user' do
     assert_equal @user1, @report.user
+  end
+
+# 追加するテスト
+  test 'should create mentions after save' do
+    # メンションのあるコンテンツを設定
+    mentioned_report = reports(:another_report) # 適切なフィクスチャを使用
+    @report.content = "Mentioning http://localhost:3000/reports/#{mentioned_report.id}"
+
+    # 保存してメンションが作成されるか確認
+    assert_difference('@report.mentioning_reports.count', 1) do
+      @report.save
+    end
+
+    # 期待するメンションが含まれているか確認
+    assert_includes @report.mentioning_reports, mentioned_report
+  end
+
+  test 'should handle mentions that reference non-existent reports' do
+    @report.content = "Mentioning http://localhost:3000/reports/999999"
+    @report.save
+
+    assert_empty @report.mentioning_reports
+  end
+
+  test 'should create multiple mentions' do
+    mentioned_report1 = reports(:another_report)
+    mentioned_report2 = reports(:yet_another_report)
+    @report.content = "Mentioning http://localhost:3000/reports/#{mentioned_report1.id} and http://localhost:3000/reports/#{mentioned_report2.id}"
+    @report.save
+
+    assert_includes @report.mentioning_reports, mentioned_report1
+    assert_includes @report.mentioning_reports, mentioned_report2
   end
 end
